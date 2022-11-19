aa6c4d5cc8591877f36e647e214ad6a760ac771a
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:07:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 # Build the project
 echo "Building the project..."
-pythoN3.10 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run the project
 echo "Make migrations"
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect static files"
-python3.10 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
