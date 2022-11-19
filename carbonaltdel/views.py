c0c3f7128414dc1b9bd027e90a0395ead6de2feb
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 23:25:07 +0000

    Update views.py

--- carbonaltdel/views.py
+++ carbonaltdel/views.py
@@ -3,7 +3,5 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class HomeView(APIView):
-    
-        def get(self, request, format=None):
-    
-            return Response('Hello, this is the Django REST API for www.carbonaltdel.com')
+    def get(self, request, format=None):
+        return Response('Hello, this is the Django REST API for www.carbonaltdel.com')
