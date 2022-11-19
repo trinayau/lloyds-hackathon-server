d70b77c09c840c8f44d36be3192b0e4b9399df18
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:21:40 +0000

    edit vercel and settings deploy

--- carbonaltdel/carbonaltdel/settings.py
+++ carbonaltdel/carbonaltdel/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "carbonaltdel" / "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
