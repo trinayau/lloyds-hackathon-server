f8bc0c2a7052bac869ca60f275c32b349577f1ef
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:52:55 +0000

    Update wsgi.py

--- carbonaltdel/wsgi.py
+++ carbonaltdel/wsgi.py
@@ -13,5 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carbonaltdel.settings')
 
-app = get_wsgi_application()
-app = application
+application = get_wsgi_application()
