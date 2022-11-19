342d8ca5e71ff369a2f87677061638037c3a97c2
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:40:58 +0000

    take files out of folder

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ python3.9 -m pip install -r requirements.txt
 
 # Run the project
 echo "Make migrations"
-cd carbonaltdel
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
