4dd82d34d0d8b1a20cbabbf9e77f1bc644969319
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:10:12 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,5 @@ python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect static files"
-cd carbonaltdel
 python3.9 manage.py collectstatic --noinput --clear
 
