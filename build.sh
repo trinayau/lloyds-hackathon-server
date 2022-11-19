2ec1fe1512ef13fb56a433b005e00504821c00dd
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-19 22:09:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ python3.9 -m pip install -r requirements.txt
 
 # Run the project
 echo "Make migrations"
+cd carbonaltdel
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect static files"
+cd carbonaltdel
 python3.9 manage.py collectstatic --noinput --clear
 
