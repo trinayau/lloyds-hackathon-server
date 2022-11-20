09712b6ff1dfc5d6c3643872efeef88dc475fa3e
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-20 16:16:21 +0000

    add minimum price and offset price to categories

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Product
+from django.db.models import Min
 
 class ProductSerializer(serializers.ModelSerializer):
     emission_calculated = serializers.SerializerMethodField('_emission_calculated')
@@ -15,7 +16,15 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+    minimum_product_price = serializers.SerializerMethodField('_minimum_product_price')
+    minimum_offset_price = serializers.SerializerMethodField('_minimum_offset_price')
+
+    def _minimum_product_price(self, category_object):
+        return category_object.products.aggregate(Min('price'))['price__min']
+
+    def _minimum_offset_price(self, category_object):
+        return category_object.products.aggregate(Min('offset'))['offset__min']
     class Meta:
         model = Category
-        fields = ('id', 'name', 'slug', 'description','image_url','products')
+        fields = ('id', 'name', 'slug', 'description','image_url','products', 'minimum_product_price', 'minimum_offset_price')
 
