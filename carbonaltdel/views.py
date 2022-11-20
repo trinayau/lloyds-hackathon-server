589f44ad45a8902b0056ac04d307ac5504c10002
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-20 17:51:03 +0000

    redo static settings

--- carbonaltdel/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello! You're at the wwww.carbonaltdel.com Django REST API. In the future, you will not be able to access this page. Please use the API endpoints instead.")
