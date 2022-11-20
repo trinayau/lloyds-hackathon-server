aa82ecd54bd468aac49e859284e5ae01d7bec665
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-20 19:47:50 +0000

    add cheapest product to category

--- products/serializers.py
+++ products/serializers.py
@@ -27,13 +27,21 @@ class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
     minimum_product_price = serializers.SerializerMethodField('_minimum_product_price')
     minimum_offset_price = serializers.SerializerMethodField('_minimum_offset_price')
+    cheapest_product = serializers.SerializerMethodField('get_cheapest_product')
 
     def _minimum_product_price(self, category_object):
         return category_object.products.aggregate(Min('price'))['price__min']
 
     def _minimum_offset_price(self, category_object):
         return category_object.products.aggregate(Min('offset'))['offset__min']
+
+    # get cheapest product in a category and return product as json object
+    def get_cheapest_product(self, category_object):
+        cheapest = category_object.products.order_by('price').first()
+        data = ProductSerializer(cheapest).data
+        return data
+
     class Meta:
         model = Category
-        fields = ('id', 'name', 'slug', 'description','image_url','products', 'minimum_product_price', 'minimum_offset_price')
+        fields = ('id', 'name', 'slug', 'description','image_url','products', 'minimum_product_price', 'minimum_offset_price', 'cheapest_product')
 
