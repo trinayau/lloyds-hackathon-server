65fd2a5366baaaa5e0113ee9703de7213735d301
Trina <92634994+trinayau@users.noreply.github.com>
2022-11-20 12:37:26 +0000

    change urls

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,6 @@ from products import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
-    path('products/<pk:id>/', views.ProductDetail.as_view()),
     path('products/', views.ProductList.as_view()),
     path('categories/', views.CategoryList.as_view()),
     path('categories/<slug:category_slug>/', views.CategoryDetail.as_view()),
